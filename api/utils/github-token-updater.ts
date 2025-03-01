ef3e240a0dc4cbdd796c1b802a9f5327c21a30d5
tuannhat24 <tuannhat124@gmail.com>
2025-03-01 14:54:06 +0700

    .

--- api/utils/github-token-updater.ts
+++
@@ -1,8 +0,0 @@
-export const changToNextGitHubToken = function (currentIndex: number) {
-    const tokenName = `GITHUB_TOKEN_${currentIndex + 1}`;
-    console.log(`Change to ${tokenName}`);
-    process.env.GITHUB_TOKEN = process.env[tokenName];
-    if (!process.env.GITHUB_TOKEN) {
-        throw new Error('No more GITHUB_TOKEN can be used');
-    }
-};

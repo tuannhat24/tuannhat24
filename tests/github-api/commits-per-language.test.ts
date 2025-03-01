46b630c9f866ac89d00d2890925c8f15ae42d490
tuannhat24 <tuannhat124@gmail.com>
2025-03-01 14:51:38 +0700

    new profile

---
+++ tests/github-api/commits-per-language.test.ts
@@ -0,0 +1,113 @@
+import {getCommitLanguage} from '../../src/github-api/commits-per-language';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+const mock = new MockAdapter(axios);
+
+const data = {
+    data: {
+        user: {
+            contributionsCollection: {
+                commitContributionsByRepository: [
+                    {
+                        repository: {
+                            primaryLanguage: {
+                                name: 'Rust',
+                                color: '#dea584'
+                            }
+                        },
+                        contributions: {
+                            totalCount: 99
+                        }
+                    },
+                    {
+                        repository: {
+                            primaryLanguage: {
+                                name: 'JavaScript',
+                                color: '#f1e05a'
+                            }
+                        },
+                        contributions: {
+                            totalCount: 84
+                        }
+                    },
+                    {
+                        repository: {
+                            primaryLanguage: {
+                                name: 'Rust',
+                                color: '#dea584'
+                            }
+                        },
+                        contributions: {
+                            totalCount: 100
+                        }
+                    },
+                    {
+                        repository: {
+                            primaryLanguage: {
+                                name: 'Jupyter Notebook',
+                                color: '#f18e33'
+                            }
+                        },
+                        contributions: {
+                            totalCount: 75
+                        }
+                    },
+                    {
+                        repository: {
+                            primaryLanguage: null
+                        },
+                        contributions: {
+                            totalCount: 100
+                        }
+                    }
+                ]
+            }
+        }
+    }
+};
+
+const error = {
+    errors: [
+        {
+            type: 'NOT_FOUND',
+            path: ['user'],
+            locations: [],
+            message: 'GitHub api failed'
+        }
+    ]
+};
+
+afterEach(() => {
+    mock.reset();
+});
+
+describe('commit contributions on github', () => {
+    it('should get correct commit contributions', async () => {
+        mock.onPost('https://api.github.com/graphql').reply(200, data);
+        const totalContributions = await getCommitLanguage('vn7n24fzkq', []);
+        expect(totalContributions).toEqual({
+            languageMap: new Map([
+                ['Rust', {color: '#dea584', count: 199, name: 'Rust'}],
+                ['JavaScript', {color: '#f1e05a', count: 84, name: 'JavaScript'}],
+                ['Jupyter Notebook', {color: '#f18e33', count: 75, name: 'Jupyter Notebook'}]
+            ])
+        });
+    });
+
+    it('should throw error when api failed', async () => {
+        mock.onPost('https://api.github.com/graphql').reply(200, error);
+        await expect(getCommitLanguage('vn7n24fzkq', [])).rejects.toThrow('GitHub api failed');
+    });
+
+    it('should do a case-insensitive comparison for language exclusion', async () => {
+        mock.onPost('https://api.github.com/graphql')
+            .reply(200, data);
+        const repoData = await getCommitLanguage('vn7n24fzkq', ['jupyter notebook']);
+        expect(repoData).toEqual({
+            languageMap: new Map([
+                ['Rust', {color: '#dea584', count: 199, name: 'Rust'}],
+                ['JavaScript', {color: '#f1e05a', count: 84, name: 'JavaScript'}]
+            ])
+        });
+    });
+});
